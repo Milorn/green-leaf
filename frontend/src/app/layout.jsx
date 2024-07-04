98ca4146d5566cc89b0453b1fd138636980cdfbf
Karim Ouadhi <karim.ouadhi.dz@outlook.com>
2024-07-04 10:41:50 +0100

    Landing page footer and responsive

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,6 +1,8 @@
 
 import Navbar from "@/components/Navbar";
 import "./globals.css";
+import { FiFacebook, FiInstagram, FiTwitter } from "react-icons/fi";
+import Footer from "@/components/Footer";
 
 
 export const metadata = {
@@ -12,10 +14,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Navbar/>
-        <main className="w-3/4 mx-auto my-20">
+
+        <Navbar />
+        <div className="hidden fixed top-0 right-0 min-h-screen w-[80px] bg-[#475F45] md:flex flex-col justify-center items-center gap-20">
+          <FiFacebook className="size-5 text-white" />
+          <FiInstagram className="size-5 text-white" />
+          <FiTwitter className="size-5 text-white" />
+        </div>
+        <main className="w-3/4 mx-auto my-8 md:my-20">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,26 +1,28 @@
-import {MagnifyingGlassIcon, ShoppingCartIcon, UserIcon} from "@heroicons/react/24/outline";
+import {Bars3Icon, MagnifyingGlassIcon, ShoppingCartIcon, UserIcon} from "@heroicons/react/24/outline";
 
 export default function Navbar() {
     return (
-        <header className="py-4">
+        <header className="py-8 md:py-4">
                 <nav className="flex justify-between items-center w-3/4 mx-auto">
                     <div className="flex items-center gap-2">
                         <img className="w-12 h-auto" src="/logo.png" alt="GreenLeaf logo" />
                         <h1 className="text-xl font-bold text-[#475F45]">GreenLeaf</h1>
                     </div>
 
-                    <ul>
+                    <ul className="hidden md:block">
                         <li className="inline-block mx-3 text-md text-[#6F836E] font-medium">Home</li>
                         <li className="inline-block mx-3 text-md text-[#9FA39F] font-medium">Services</li>
                         <li className="inline-block mx-3 text-md text-[#9FA39F] font-medium">Plants</li>
                         <li className="inline-block mx-3 text-md text-[#9FA39F] font-medium">Checkout</li>
                     </ul>
 
-                    <div className="flex gap-10 items-center">   
+                    <div className="hidden md:flex gap-10 items-center">   
                            <UserIcon className="size-5 text-[#475F45]"/>
                            <MagnifyingGlassIcon className="size-5 text-[#475F45]"/>
                            <ShoppingCartIcon className="size-5 text-[#475F45]"/>
                     </div>
+
+                    <Bars3Icon className="block md:hidden size-8 text-[#475F45]"/>
                 </nav>
         </header>
     )
